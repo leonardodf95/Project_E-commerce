262bdb4bb591b84207b6e858284576b25a310e77
Leonardo Dias Ferreira <leonardodf95@gmail.com>
2022-06-16 17:34:32 -0300

    Tela de cadastro finalizada

--- e-commerce/src/screens/cadastro/index.jsx
+++ e-commerce/src/screens/cadastro/index.jsx
@@ -1,13 +1,29 @@
-import { Form, Input, DatePicker } from "antd"
-import { Container } from "./style";
+import { Form, Input, DatePicker, Button } from "antd"
+import { Container, SubTitle, Title, TitleContainer } from "./style";
 
 const Cadastro = () => {
     const [form] = Form.useForm()
     return (
         <Container>
-           <Form form={form}>
-                <Form.Item label='Nome'>
-                    <Input/>
+            <TitleContainer>
+                <Title>Cadastro</Title>
+                <SubTitle>* Campos obrigatórios.</SubTitle>
+            </TitleContainer>
+           <Form layout="vertical" form={form}>
+                <Form.Item label='* Nome:'>
+                    <Input placeholder="* Nome"/>
+                </Form.Item>
+                <Form.Item label='* E-mail:'>
+                    <Input placeholder="* E-mail" type='email' />
+                </Form.Item> 
+                <Form.Item label='* Senha:'>
+                    <Input placeholder='* Senha' type='password'/>
+                </Form.Item>
+                <Form.Item label='Data de Nascimento:'>
+                    <DatePicker placeholder="Data de Nascimento"/>
+                </Form.Item>
+                <Form.Item>
+                    <Button>Cadastrar</Button>
                 </Form.Item>
            </Form>
         </Container>
--- e-commerce/src/screens/cadastro/style.jsx
+++ e-commerce/src/screens/cadastro/style.jsx
@@ -2,11 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
     justify-content: center;
     width: 30vw;
     margin: auto;
     margin-top: 50px;
     padding: 20px;
     background-color: #fff;
+`
+
+
+
+
+export const TitleContainer = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    margin-bottom: 10px;
+`
+
+export const Title = styled.h1`
+    margin: 0;
+`
+
+export const SubTitle = styled.span`
+    font-weight: 300;
+    font-size: 12px;
+    margin-bottom: 5px;
 `
